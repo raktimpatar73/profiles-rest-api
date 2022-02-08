a2abd949c38d4f3cef5b965e1ea156cf10b589c4
Raktim Patar <raktimpatar.73@gmail.com>
2022-02-08 12:50:42 +0530

    changed profiles_api.id_staff to profiles_api.is_staff

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -34,7 +34,7 @@ class UserProfile( AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    id_staff = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
